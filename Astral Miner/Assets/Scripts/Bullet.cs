fdd05722e1073ff49d022af169a5bacd45511955
André Tavares <andre.t.teixeira@hotmail.com>
2026-09-10 15:13:54 -0300

    Enemy start

---
+++ Astral Miner/Assets/Scripts/Bullet.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Enemy"))
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+
+        if(collision.CompareTag("Asteroid"))
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+            //add to score/money
+
+        }
+    }
+
+
+
+
+
+}
